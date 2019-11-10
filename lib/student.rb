5ec01c5bb624a50f7fd59bd3f54436e5548d4e00
Amgad Malik <amgadmalik@yahoo.com>
2019-11-10 14:42:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    #:name = student_hash[:name]
+    name = student_hash[:name]
+    location = student_hash[:location]
     binding.pry
   end
 
